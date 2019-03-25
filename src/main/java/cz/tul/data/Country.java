274c6ca71dded62c5ecfae51a2e086283b96c238
Lukáš Pícha <lukas.picha@novy-web.cz>
2019-03-25 14:36:41 +0100

    init project

---
+++ src/main/java/cz/tul/data/Country.java
@@ -0,0 +1,47 @@
+package cz.tul.data;
+
+
+public class Country {
+
+    private String code;
+    private String name;
+
+    public Country() {
+
+    }
+
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country[code=" + this.code + ", name=" + this.name + "]";
+    }
+
+    public JsonObject toJson() {
+        JsonObject obj = new JsonObject();
+        obj.put("code", this.code);
+        obj.put("name", this.name);
+        return obj;
+    }
+
+
+}
