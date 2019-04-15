8373e0b2845aec4329fe8542ac7a6b26bea4e83d
Lukáš Pícha <lukas.picha@novy-web.cz>
2019-04-15 19:32:09 +0200

    add CountriesServiceTests for all crud actions

---
+++ src/test/java/cz/tul/CountriesServiceTests.java
@@ -0,0 +1,66 @@
+package cz.tul;
+
+import cz.tul.data.Country;
+import cz.tul.data.User;
+import cz.tul.data.UsersDao;
+import cz.tul.service.CountryService;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {Main.class})
+@ActiveProfiles({"devel"})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class CountriesServiceTests {
+
+    @Autowired
+    private CountryService countryService;
+
+
+    @Test
+    public void TestInsertAndGet() {
+        Country country_pol = new Country("POL", "Polsko");
+
+        this.countryService.create(country_pol);
+        List<Country> countries = this.countryService.selectAll();
+        assertEquals("Count of countries after cretaing should be 1", 1, countries.size());
+
+
+        assertEquals("Countries should be equals", country_pol, this.countryService.getByCode("POL"));
+
+
+    }
+    @Test
+    public void TestUpdate() {
+        Country country_pol = new Country("POL", "Posko");
+        this.countryService.create(country_pol);
+
+        String correctStateName = "Polsko";
+        country_pol.setName(correctStateName);
+        this.countryService.update(country_pol);
+
+        assertEquals("Country name should be " + correctStateName, correctStateName, this.countryService.getByCode("POL").getName());
+
+
+    }
+
+    @Test
+    public void TestDeleteAll() {
+
+        this.countryService.delete();
+        List<Country> countires = this.countryService.selectAll();
+        assertEquals("Count of countries after delete should be 0", 0, countires.size());
+
+    }
+}
