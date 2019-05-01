6e2ff9a8e68d14103166120bf5ed40ad267fa6e3
Lukáš Pícha <lukas.picha@novy-web.cz>
2019-05-01 23:47:47 +0200

    todo query by agregation

--- src/main/java/cz/tul/repositories/IMeasurementRepository.java
+++ src/main/java/cz/tul/repositories/IMeasurementRepository.java
@@ -1,6 +1,8 @@
 package cz.tul.repositories;
 import cz.tul.data.Measurement;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
@@ -21,11 +23,16 @@ public interface IMeasurementRepository extends MongoRepository<Measurement, Obj
     //                     { $group: { _id: "$openWeatherMapName", pressureAvg: { $avg: "$pressure" },  humidityAvg: { $avg: "$humidity" },  tempAvg: { $avg: "$temp" } } }
     //                   ])
 
+    // Aggregation aggregation = newAggregation(
+    //                match(Criteria.where("openWeatherMapName").is(openWeatherMapName)),
+    //                match(Criteria.where("measuredTimestamp").gte(time)),
+    //                group("openWeatherMapName").avg("temp").as("tempAvg").avg("pressure").as("pressureAvg").avg("humidity").as("humidityAvg"));
+
     //prumery pro vsechna mesta od urciteho datumu
     // db.measurements.aggregate([
     //                     { $match: {  measuredTimestamp: {$gte: 1556650211 }} },
     //                     { $group: { _id: "$openWeatherMapName", pressureAvg: { $avg: "$pressure" },  humidityAvg: { $avg: "$humidity" },  tempAvg: { $avg: "$temp" } } }
     //                   ])
-
+    
 
 }
