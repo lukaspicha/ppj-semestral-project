6ab8ba6e059b4d7354b7f3d555851c7cbd87e151
Lukáš Pícha <lukas.picha@novy-web.cz>
2019-05-01 23:57:54 +0200

    prepare weather averages methods

--- src/main/java/cz/tul/data/controllers/rest/WeatherController.java
+++ src/main/java/cz/tul/data/controllers/rest/WeatherController.java
@@ -5,10 +5,12 @@ import cz.tul.service.MeasurementService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -50,5 +52,45 @@ public class WeatherController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
+    @RequestMapping(value = "/weather/averages/{id}/{averageFor}", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<List<Measurement>> getCountry(@PathVariable String id, @PathVariable String averageFor) {
+        this.logger.info("GET /weather/averages/" + id + "/" + averageFor);
+        try {
+            Date date = new Date();
+            long time = date.getTime() - this.getIntervalFromParam(averageFor);
+
+
+            AggregationResults<Measurement> list  = this.measurementService.getAveragesgetByOpenWeatherMapName("524901", 20);
+            //this.logger.info("Returning a " + measurements.size() + "measuremenets for id " + id);
+            //return new ResponseEntity<List<Measurement>>(measurements, HttpStatus.OK);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private int getIntervalFromParam(String averageFor) {
+        int interval;
+        switch(averageFor) {
+            case "last-day":
+                interval = 24 * 60 * 60;
+                break;
+            case "last-week":
+                interval = 24 * 60 * 60 * 7;
+                break;
+            case "last-half-moon":
+                interval = 24 * 60 * 60 * 14;
+                break;
+            default:
+                interval = 86400;
+                break;
+        }
+
+        return interval;
+    }
+
+
 
 }
