328c210351bd430788605d0cc47da562ac1bdf42
Lukáš Pícha <lukas.picha@novy-web.cz>
2019-05-01 20:24:41 +0200

    register feed controller as scheduled bean in main class

--- src/main/java/cz/tul/Main.java
+++ src/main/java/cz/tul/Main.java
@@ -1,10 +1,19 @@
 package cz.tul;
 
+import cz.tul.data.controllers.rest.FeedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Main {
+
+    @Bean
+    public FeedController feedController() {
+        return new FeedController();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
