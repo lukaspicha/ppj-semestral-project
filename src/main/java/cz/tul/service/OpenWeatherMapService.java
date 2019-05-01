2ea0c609f24b8abb6fdbb0f4086dfeaaf429dfdd
Lukáš Pícha <lukas.picha@novy-web.cz>
2019-05-01 20:15:00 +0200

    update OWMservice, downloading data from api

--- src/main/java/cz/tul/service/OpenWeatherMapService.java
+++ src/main/java/cz/tul/service/OpenWeatherMapService.java
@@ -2,8 +2,11 @@ package cz.tul.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import cz.tul.data.City;
 import cz.tul.data.Measurement;
-import org.omg.CORBA.Object;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
@@ -11,47 +14,66 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OpenWeatherMapService {
 
-    //TODO nacist si to z app.properties
-    protected String apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+    @Value("${openWeatherMap.apiUrl}")
+    protected String apiUrl = "https://api.openweathermap.org/data/2.5/group";
+
+    @Value("${openWeatherMap.appid}")
     protected String appId = "685d749db94d50fddfc47e2f8f28e1a8";
-    protected String units = "metric";
+
+    @Value("${openWeatherMap.units}")
+    protected String units = "metrics";
 
     protected RestTemplate restTemplate;
 
     protected UriComponentsBuilder uriBuilder;
 
-    public OpenWeatherMapService() {
 
+
+    public OpenWeatherMapService() {
         this.uriBuilder = UriComponentsBuilder.fromUriString(this.apiUrl);
         this.uriBuilder.queryParam("appId", this.appId);
         this.uriBuilder.queryParam("units", this.units);
         this.restTemplate = new RestTemplate();
-
     }
 
-    //TODO upravit, resp.pro kazde nove volani resetovat predchozi cityid
-    public Measurement getByCityId(String cityId) {
-        this.uriBuilder.queryParam("id", cityId);
+
+    public List<Measurement> downloadWeatherDataByCitiesIds(List<String> citiesIds) {
+        List<Measurement> measurements = new ArrayList<Measurement>();
         try {
+            this.uriBuilder.queryParam("id",  String.join(",", citiesIds));
             ResponseEntity<String> response = this.restTemplate.getForEntity(uriBuilder.toUriString(), String.class);
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode root = mapper.readTree(response.getBody());
-            JsonNode main = root.get("main");
 
-             return new Measurement(main.get("temp").floatValue(), main.get("pressure").asInt(), main.get("humidity").asInt(), root.get("dt").asLong(), this.units);
+            if(response.getStatusCode() == HttpStatus.OK) {
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode jsonResponse = mapper.readTree(response.getBody());
 
+                if(jsonResponse.get("cnt").intValue() > 0 && jsonResponse.get("list").isArray()) {
+                    for (JsonNode cityMeasData : jsonResponse.get("list")) {
+                        JsonNode cityMeasDataMain = cityMeasData.get("main");
+                        Measurement meas = new Measurement(cityMeasData.get("id").asText(), cityMeasDataMain.get("temp").floatValue(), cityMeasDataMain.get("pressure").asInt(),  cityMeasDataMain.get("humidity").asInt(), cityMeasData.get("dt").asLong(), this.units);
+                        measurements.add(meas);
+                    }
+                }
+                //LOG
+
+            } else {
+
+               //LOG
+            }
         } catch (HttpStatusCodeException e) {
             int statusCode = e.getStatusCode().value();
-            //return e.getMessage();
+            System.out.println(e.getStatusText());
         } catch (IOException e) {
-            //return e.getMessage();
+            System.out.println(e.getMessage());
         }
 
-        return null;
+        return measurements;
 
     }
 
