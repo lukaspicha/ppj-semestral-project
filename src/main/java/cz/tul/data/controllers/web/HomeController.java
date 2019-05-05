83dbd2fbffaf68248bee121f23d859d1d279d0ce
Lukáš Pícha <lukas.picha@novy-web.cz>
2019-05-05 18:59:25 +0200

    [Web] getting averages for all cities in specific country

--- src/main/java/cz/tul/data/controllers/web/HomeController.java
+++ src/main/java/cz/tul/data/controllers/web/HomeController.java
@@ -1,25 +1,38 @@
 package cz.tul.data.controllers.web;
 
+import cz.tul.data.City;
 import cz.tul.data.Country;
+import cz.tul.data.WeatherData;
+import cz.tul.service.CityService;
 import cz.tul.service.CountryService;
+import cz.tul.service.MeasurementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.security.Timestamp;
+import java.util.*;
 
 
 @Controller
 public class HomeController {
 
     protected  CountryService countryService;
+    protected CityService cityService;
+    protected MeasurementService measurementService;
 
     @Autowired
     public void setCountryService(CountryService countryService) { //lecture11_springMvc
         this.countryService = countryService;
     }
+
+    @Autowired
+    public void setCityService(CityService cityService) {this.cityService = cityService;}
+
+    @Autowired
+    public void setMeasurementService(MeasurementService measurementService) {this.measurementService = measurementService;}
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String showIndex(ModelMap model) {
@@ -34,6 +47,21 @@ public class HomeController {
         Country country = this.countryService.getByCode(countryCode);
         if(country != null) {
             model.addAttribute("country", country);
+            List<City> cities = this.cityService.findByCountryCode(country.getCode());
+            Map<City, List<WeatherData>> weatherData = new HashMap<City, List<WeatherData>>();
+            long time = System.currentTimeMillis() / 1000;
+
+            for (City c : cities) {
+
+                List<WeatherData> weatherAverages = new ArrayList<WeatherData>();
+                weatherAverages.add(this.measurementService.getAveragesByOpenWeatherMapNameAndTimeGreaterThan(c.getOpenWeatherMapName(), time - 3600));
+                weatherAverages.add(this.measurementService.getAveragesByOpenWeatherMapNameAndTimeGreaterThan(c.getOpenWeatherMapName(), time - 1209600));
+                weatherAverages.add(this.measurementService.getAveragesByOpenWeatherMapNameAndTimeGreaterThan(c.getOpenWeatherMapName(), time - (2 * 1209600)));
+
+                weatherData.put(c, weatherAverages);
+            }
+            model.addAttribute("weatherData", weatherData);
+            System.out.println(weatherData);
             return "home/detail";
         }
 
